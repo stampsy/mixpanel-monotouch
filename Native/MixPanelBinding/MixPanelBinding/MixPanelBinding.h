240c4c0f8fc26de2923fd7083a18c5084ab1c65b
Dan Abramov <dan.abramov@gmail.com>
2012-11-15 21:32:11 +0400

    Add native lib

---
+++ Native/MixPanelBinding/MixPanelBinding/MixPanelBinding.h
@@ -0,0 +1,14 @@
+//
+//  MixPanelBinding.h
+//  MixPanelBinding
+//
+//  Created by Alexander Kuznetsov on 08.11.12.
+//  Copyright (c) 2012 Alexander Kuznetsov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface MixPanelBinding : NSObject
+
+@end
